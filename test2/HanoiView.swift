f5abe486b68b4124796437f1d92e5c3e7be946fe
robert@Mac <robert.jonas@gmx.at>
2017-06-15 21:14:23 +0200

    Base after deleting unnecessary code

---
+++ test2/HanoiView.swift
@@ -0,0 +1,29 @@
+//
+//  HanoiView.swift
+//  test2
+//
+//  Created by Robert Jonas on 15.06.17.
+//  Copyright © 2017 jr-soft. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class HanoiView: SKScene {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        
+    }
+    
+    
+}
