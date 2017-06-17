6f8a044ae14d134ca3db5967457455bc625fa87d
robert@Mac <robert.jonas@gmx.at>
2017-06-17 16:00:59 +0200

    Zwischenstand

--- test2/HanoiView.swift
+++ test2/HanoiView.swift
@@ -13,6 +13,14 @@ class HanoiView: SKScene {
     let towerA=0
     let towerB=1
     let towerC=2
+    
+    var disks:[SKSpriteNode]=[]
+    var towerHeight=[0,0,0]
+    
+    var towerPos=[CGFloat(0),CGFloat(0),CGFloat(0)]
+    
+    
+    
     var towers:[[SKSpriteNode]]=[[]]
     var disk1Width=5
     let diskHeight=10
@@ -24,8 +32,9 @@ class HanoiView: SKScene {
         super.init(size: size)
         
         disk1Width=Int(self.frame.width-4*5)/4/10
-        
-        
+        towerPos[towerA]=self.frame.width/4
+        towerPos[towerB]=self.frame.width/2
+        towerPos[towerC]=self.frame.width*3/4
         
         
         
@@ -53,7 +62,7 @@ class HanoiView: SKScene {
             self.addChild(towers[towerA][i])
             
             //let disk fall onto stake
-     //       let delay=SKAction.wait(forDuration: 2)
+            //       let delay=SKAction.wait(forDuration: 2)
             let moveDiskDown=SKAction.moveTo(y: CGFloat((50+diskHeight/2)+i*diskHeight), duration: 2)
             let seq=SKAction.sequence([moveDiskDown])
             towers[towerA][i].run(seq)
@@ -66,7 +75,15 @@ class HanoiView: SKScene {
         
     }
     //---------------------------------------------------------------------------------------------------------------
-    func drawTowers(){
+    func moveDisk(diskNo: Int,from: Int,to: Int){
+        let moveDiskUp=SKAction.moveTo(y: 50, duration: 2)
+        towerHeight[from]-=1
+        let moveDiskHor=SKAction.moveTo(x: towerPos[to], duration: 1)
+        let moveDiskDown=SKAction.moveTo(y: CGFloat(50+towerHeight[to]*diskHeight), duration: 2)
+        towerHeight[to]+=1
+        
+        let seq=SKAction.sequence([moveDiskUp,moveDiskHor,moveDiskDown])
+        disks[diskNo].run(seq)
         
         
     }
